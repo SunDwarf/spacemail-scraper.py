22dc5a19d4710de6c1c37d7dde3239cb9408fdc5
Laura <l@veriny.tf>
2017-04-14 00:09:39 +0100

    Make scraper more error resistant.

--- spacemail.py
+++ spacemail.py
@@ -1,22 +1,23 @@
-import requests
-import threading
-from bs4 import BeautifulSoup
-
 import sys
+import threading
 
+import os
+import requests
+from bs4 import BeautifulSoup
 from dateutil import parser
-from sqlalchemy import create_engine, Column, Integer, String, Table, DateTime, exists
+from json import JSONDecodeError
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, exists
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('postgresql://postgres@localhost:5432/spacemail')
 session_factory = sessionmaker(bind=engine)
 Session = scoped_session(session_factory)
 
-
-
 Base = declarative_base()
+
+DEBUG = os.environ.get("SP_DEBUG", "true") != "false"
 
 
 class Post(Base):
@@ -28,7 +29,7 @@ class Post(Base):
     date = Column(DateTime)
 
 
-def main(thread_id: int):
+def main(thread_id: int, debug: bool = True):
     # Postgres due to threads
 
     session = Session()
@@ -38,12 +39,12 @@ def main(thread_id: int):
         "Referer": "http://space.galaxybuster.net/go.php",
         "Origin": "http://space.galaxybuster.net"}
     while True:
-        print("[Thread-{}] Retrieving message...".format(thread_id))
+        debug and print("[Thread-{}] Retrieving message...".format(thread_id))
         # Get header
         try:
             msg_header = sess.get("http://space.galaxybuster.net/lib/get.php")
         except:
-            print("[Thread-{}] - Failed to get new message".format(thread_id))
+            debug and print("[Thread-{}] - Failed to get new message".format(thread_id))
             continue
         # Parse it
         soup = BeautifulSoup(msg_header.content, "html.parser")
@@ -53,30 +54,46 @@ def main(thread_id: int):
         if divs:
             # Get ID
             mid = divs[0]["data-id"]
-            print("[Thread-{}] - ID:".format(thread_id), mid)
+            debug and print("[Thread-{}] - ID:".format(thread_id), mid)
 
             if not session.query(exists().where(Post.id == mid)).scalar():
                 # Get message data
-                msg = sess.post("http://space.galaxybuster.net/lib/view.php", data={"id": mid})
+                try:
+                    msg = sess.post("http://space.galaxybuster.net/lib/view.php", data={"id": mid})
+                except:
+                    debug and print("Thread-{}] -".format(thread_id), "ID: {} -".format(mid),
+                                    "Error fetching new post body.")
+
+                try:
+                    data = msg.json()[0]
+                except JSONDecodeError:
+                    debug and print("[Thread-{}] - ID: {} - Error decoding body ({})"
+                                    .format(thread_id, mid, msg.text))
+                    continue
+
                 # Parse it
-                msg_soup = BeautifulSoup(msg.json()[0], "html.parser")
-                subject = msg_soup.find(id="msgSubject")
-                sender = msg_soup.find(id="msgSender")
-                body = msg_soup.find(id="msgBody")
-                date = msg_soup.find(id="msgDate")
-                print("[Thread-{}] - Subject:".format(thread_id), subject.getText())
-                print("[Thread-{}] - From:".format(thread_id), sender.getText())
-                print("[Thread-{}] - Body:".format(thread_id), body.getText())
-                print("[Thread-{}] - Date:".format(thread_id), date.getText(),
-                      "Formatted:", parser.parse(date.getText()))
-                p = Post(id=mid, sender=sender.getText(), body=body.getText(), subject=subject.getText(),
-                         date=parser.parse(date.getText()))
-                session.add(p)
+                try:
+                    msg_soup = BeautifulSoup(data, "html.parser")
+                    subject = msg_soup.find(id="msgSubject")
+                    sender = msg_soup.find(id="msgSender")
+                    body = msg_soup.find(id="msgBody")
+                    date = msg_soup.find(id="msgDate")
+                    debug and print("[Thread-{}] - Subject:".format(thread_id), subject.getText())
+                    debug and print("[Thread-{}] - From:".format(thread_id), sender.getText())
+                    debug and print("[Thread-{}] - Body:".format(thread_id), body.getText())
+                    debug and print("[Thread-{}] - Date:".format(thread_id), date.getText(),
+                                    "Formatted:", parser.parse(date.getText()))
+                    p = Post(id=mid, sender=sender.getText(), body=body.getText(),
+                             subject=subject.getText(),
+                             date=parser.parse(date.getText()))
+                    session.add(p)
+                except:
+                    print("[Thread-{}] -".format(thread_id), "Bad HTML body returned.")
             else:
-                print("[Thread-{}] - Already seen before.".format(thread_id))
+                debug and print("[Thread-{}] - Already seen before.".format(thread_id))
 
         else:
-            print("[Thread-{}] - Returned empty.".format(thread_id))
+            debug and print("[Thread-{}] - Returned empty.".format(thread_id))
         session.commit()
 
 
@@ -88,5 +105,5 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     print("Starting {} threads...".format(threads))
     for i in range(0, threads):
-        th = threading.Thread(target=main, args=(i,))
+        th = threading.Thread(target=main, args=(i, DEBUG))
         th.start()
